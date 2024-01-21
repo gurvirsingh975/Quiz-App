af1355ef3b8a39ea0427022d63be2436d7533fcf
singh Gurvir <gurvirsingh975@gmail.com>
2024-01-21 13:59:42 -0400

    added js questions in script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const questions = [
+    {
+        question: "What is JavaScript?",
+        answers: ["A programming language", "A fruit", "A country", "A car"],
+        correctIndex: 0,
+    },
+    { Question: 'Inside which HTML element do we put the JavaScript?' ,
+      answers:[ '<scripting>' ,'<js>','<JavaScript>' ,'script' ],
+      correctIndex:3 ,
+},
+{ Question: 'What is the correct syntax for referring to an external script called "xxx.js"? ',
+ answers:['Script src="xxx.js" ','Script href="xxx.js" ','Script name="xxx.js" ','Script ="xxx.js" '],
+   correctIndex: 0 ,
+},
+{ Question: 'How do you write "Hello World" in an alert box?' ,
+answers:['Alert = "Hello World" ','alertBox("Hello World")','alert("Hello World")' ,'window.alert("Hello World")'],
+ correctIndex: 1,
+
+},
+{ Question:'How do you create a function in JavaScript?' ,
+answers:[ 'function.myfuction()','function=myfunction()','function myfunction()' , '()myfunction: function'],
+ correctIndex: 2,
+},
+
+
+];
